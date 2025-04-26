e2b655507c0eb6bcc10658e340dfc4a9456a9b3b
Kwanyeob <52995390+Kwanyeob@users.noreply.github.com>
2025-04-25 22:35:17 -0400

    Add files via upload
    basic java spring repo

---
+++ Qode/src/test/java/com/app/Qode/QodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Qode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
